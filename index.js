bf2e8a56802d70f1a52b2a4dbca5ca075b0a9d98
Mark <hc.devmark@gmail.com>
2017-07-26 16:04:27 +0800

    bug fixs

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 
 const Validator = require('./src/Validator');
 const Sanitization = require('./src/Sanitization');
+const Transform = require('./src/Transforms');
 
 module.exports = {
   validate: Validator.validate,
   validateAll: Validator.validateAll,
   extend: Validator.extend,
   extendImplicit: Validator.extendImplicit,
+
   is: Validator.is,
   'is.extend': Validator.is.extend,
   sanitize: Sanitization.sanitize,
   sanitizor: Sanitization.sanitizor,
   sanitizeAll: Sanitization.sanitizeAll,
-  'sanitizor.extend': Sanitization.sanitizor.extend
+  'sanitizor.extend': Sanitization.sanitizor.extend,
+
+  transform: Validator.Transform,
 };
